25a531bca52b2365425b899ce36903fe0195dead
sodole <lws0915@gmail.com>
2022-12-13 22:54:28 +0900

    실험 완료

--- json-server/main.js
+++ json-server/main.js
@@ -1,6 +1,9 @@
-const fs = require("fs");
-// fetch("http://localhost:3000/useok")
-//   .then((response) => response.json())
-//   .then((json) => console.log(json));
-const dbs = fs.readFileSync("./db.json", "utf8");
-console.log(dbs);
+const ul = document.getElementById("ul");
+const Createli = document.createElement("li");
+
+fetch("http://localhost:3000/useok")
+  .then((response) => response.json())
+  .then((json) => {
+    ul.innerHTML = json.name;
+    ul.Createli.innerHTML = json.name;
+  });
